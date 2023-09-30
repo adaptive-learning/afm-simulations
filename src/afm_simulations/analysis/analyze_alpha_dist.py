e0e38c9febbac962adc2f19e4701d938a9830880
Jaroslav Čechák <xcechak1@fi.muni.cz>
2023-09-30 13:23:04 +0200

    feature: add first analysis script to reproduce figure 4.2

--- src/afm_simulations/analysis/analyze_alpha_dist.py
+++ src/afm_simulations/analysis/analyze_alpha_dist.py
@@ -1,11 +1,15 @@
+"""Reproduces Figure 4.2"""
+
 from itertools import repeat
 from pathlib import Path
 
 import matplotlib
 import matplotlib.pyplot as plt
 import pandas as pd
-import seaborn as sns
-from afm_tf2 import common
+import seaborn as sns  # type: ignore
+
+from afm_simulations.afm.common import fit_all_scenarios, load_scenarios_params
+from afm_simulations.simulations.common import run_simulation
 
 matplotlib.use("agg")
 
@@ -18,9 +22,9 @@ EXPORT_PATH = "fitted_params_tf.csv"
 EXCLUDED_ITEMS = "excluded_items"
 
 SCENARIO_BASE_PROPER_NAMES = {
-    "debug_cheating_combined_thirds_gt_alpha": "⅓ overlap GT $\\alpha$ ",
-    "debug_cheating_combined_thirds": "⅓ overlap",
-    "debug_cheating_combined_0": "no overlap",
+    "cheating_combined_thirds_gt_alpha": "⅓ overlap GT $\\alpha$ ",
+    "cheating_combined_thirds": "⅓ overlap",
+    "cheating_combined_0": "no overlap",
 }
 
 
@@ -31,16 +35,9 @@ def plot_hist(params, name, scenario=None, tag=None, separate_folds=False):
         .reset_index(drop=True)
     )
 
-    # relevant_params = relevant_params[
-    #     (~relevant_params["scenario"].str.contains("gt_alpha"))
-    #     | (relevant_params["fold"] == 1)
-    # ].reset_index(drop=True)
-
     relevant_params = relevant_params.reset_index(drop=True)
 
-    relevant_params["base_scenario_name"] = relevant_params["scenario"].apply(
-        lambda s: s[:-3]
-    )
+    relevant_params["base_scenario_name"] = relevant_params["scenario"].apply(lambda s: s[:-3])
 
     relevant_params["base_scenario_name_plus_fold"] = (
         relevant_params["base_scenario_name"] + "_" + relevant_params["fold"].apply(str)
@@ -51,42 +48,21 @@ def plot_hist(params, name, scenario=None, tag=None, separate_folds=False):
     else:
         row_faceting_col_name = "base_scenario_name"
 
-    relevant_params["base_scenario_name"] = relevant_params["base_scenario_name"].apply(
-        SCENARIO_BASE_PROPER_NAMES.get
-    )
+    relevant_params["base_scenario_name"] = relevant_params["base_scenario_name"].apply(SCENARIO_BASE_PROPER_NAMES.get)
 
     if "cheating_prevalence" in relevant_params.columns:
-        relevant_params["cheating_prevalence"] = (
-            relevant_params["cheating_prevalence"] * 100
-        ).astype(int)
+        relevant_params["cheating_prevalence"] = (relevant_params["cheating_prevalence"] * 100).astype(int)
 
-        relevant_params["cheater"] = relevant_params["cheater"].apply(
-            lambda x: "Yes" if x else "No"
-        )
-
-        # g = sns.displot(
-        #     data=relevant_params,
-        #     x="value",
-        #     hue="cheater",
-        #     hue_order=["No", "Yes"],
-        #     multiple="stack",
-        #     kde=True,
-        #     col="cheating_prevalence",
-        #     row=row_faceting_col_name,
-        #     kind="hist",
-        #     bins=30,
-        # )
+        relevant_params["cheater"] = relevant_params["cheater"].apply(lambda x: "Yes" if x else "No")
 
         g = sns.FacetGrid(
             relevant_params,
             col="cheating_prevalence",
             row=row_faceting_col_name,
-            # row_order=SCENARIO_BASE_PROPER_NAMES.items(),
             legend_out=True,
             sharex="col",
             sharey="none",
             margin_titles=True,
-            # height=5,
             aspect=1.2,
             gridspec_kws={"wspace": 0.1, "hspace": 0.1},
         )
@@ -104,12 +80,8 @@ def plot_hist(params, name, scenario=None, tag=None, separate_folds=False):
         # The color cycles are going to all the same, doesn't matter which axes we use
         ax = g.axes[0][1]
 
-        # Some how for a plot of 5 bars, there are 6 patches, what is the 6th one?
-        boxes = [
-            item
-            for item in ax.get_children()
-            if isinstance(item, matplotlib.patches.Rectangle)
-        ][:-1]
+        # Somehow for a plot of 5 bars, there are 6 patches, what is the 6th one?
+        boxes = [item for item in ax.get_children() if isinstance(item, matplotlib.patches.Rectangle)][:-1]
 
         # There is no labels, need to define the labels
         legend_labels = ["No", "Yes"]
@@ -132,9 +104,7 @@ def plot_hist(params, name, scenario=None, tag=None, separate_folds=False):
         )
         g.set(yticks=[])
 
-        for excluded_cheater_value, color, linestyle in zip(
-            ["No", "Yes", 10], ["C1", "C0", "red"], repeat("--")
-        ):
+        for excluded_cheater_value, color, linestyle in zip(["No", "Yes", 10], ["C1", "C0", "red"], repeat("--")):
             # value 10 is a hack to include both cheaters and non-cheaters
             for base in g.row_names:
                 for prevalence in g.col_names:
@@ -144,15 +114,11 @@ def plot_hist(params, name, scenario=None, tag=None, separate_folds=False):
                         & (relevant_params["cheater"] != excluded_cheater_value)
                     ]["value"].mean()
 
-                    g.axes_dict[(base, prevalence)].axvline(
-                        x=mean, color=color, linestyle=linestyle, linewidth=2
-                    )
+                    g.axes_dict[(base, prevalence)].axvline(x=mean, color=color, linestyle=linestyle, linewidth=2)
 
-        # g.set_axis_labels(f"Value of parameter {name}", f"Estimated density")
-        g.set_axis_labels(f"$\\alpha$ value", "")
+        g.set_axis_labels("$\\alpha$ value", "")
         g.set_yticklabels()
         g.set_titles(
-            # template="{row_name} @ {col_name} % cheaters",
             col_template="{col_name} % cheaters",
             row_template="{row_name}",
         )
@@ -163,24 +129,22 @@ def plot_hist(params, name, scenario=None, tag=None, separate_folds=False):
         )
 
     if scenario is not None:
-        plt.savefig(f"fig/{scenario}/{name}_dist.svg")
+        image_name = f"fig/{scenario}/{name}_dist.svg"
+        plt.savefig(image_name)
     else:
+        image_name = f"fig/cheating_{name}_dist{'_' + tag if tag else ''}.svg"
         plt.savefig(
-            f"fig/cheating_{name}_dist{'_' + tag if tag else ''}.svg",
+            image_name,
             bbox_inches="tight",
         )
-    # plt.show()
     plt.clf()
+    print(f"Saved image to {image_name}")
 
 
 def analyze_alpha_distributions(params_df: pd.DataFrame) -> None:
-    for _, (betas, gammas) in list(
-        params_df[["betas", "gammas"]].drop_duplicates().iterrows()
-    ):
+    for _, (betas, gammas) in list(params_df[["betas", "gammas"]].drop_duplicates().iterrows()):
         tag = f"{betas}-{gammas}"
-        relevant_params_df = params_df[
-            (params_df["betas"] == betas) & (params_df["gammas"] == gammas)
-        ]
+        relevant_params_df = params_df[(params_df["betas"] == betas) & (params_df["gammas"] == gammas)]
         plot_hist(
             relevant_params_df,
             "alpha",
@@ -189,6 +153,17 @@ def analyze_alpha_distributions(params_df: pd.DataFrame) -> None:
 
 
 if __name__ == "__main__":
-    # common.fit_all_in_path(Path("./data"))
-    params_df = common.load_params(Path("./data"))
+    run_simulation(
+        [
+            "../scenarios/cheating_combined_0.json",
+            "../scenarios/cheating_combined_thirds_simple.json",
+        ]
+    )
+
+    relevant_scenarios = list(list(Path().glob("data/cheating_combined_thirds_[025g]*"))) + list(
+        Path().glob("data/cheating_combined_0*")
+    )
+
+    fit_all_scenarios(relevant_scenarios)
+    params_df = load_scenarios_params(relevant_scenarios)
     analyze_alpha_distributions(params_df)
